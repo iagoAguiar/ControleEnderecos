e2359ab42ebfa7e22cc13fd2f7ec6d6a63edfe54
iagoAguiar <iagoaguiar202@gmail.com>
2021-07-31 15:58:33 -0300

    Trabalhando com o ViaCEP

--- src/main/java/com/CadastroUsuarios/Register/RegisterApplication.java
+++ src/main/java/com/CadastroUsuarios/Register/RegisterApplication.java
@@ -2,8 +2,10 @@ package com.CadastroUsuarios.Register;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class RegisterApplication {
 
 	public static void main(String[] args) {
