e349015fa6abad0521101250139100bd5808ea62
iagoAguiar <iagoaguiar202@gmail.com>
2021-09-19 10:59:03 -0300

    Alterado o Readme e adicionado o Swagger.

--- src/main/java/com/CadastroUsuarios/Register/controller/HomePageController.java
+++ src/main/java/com/CadastroUsuarios/Register/controller/HomePageController.java
@@ -1,13 +1,18 @@
 package com.CadastroUsuarios.Register.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@Api(value = "HomePage",  tags = { "HomePage" })
 public class HomePageController {
     @GetMapping
+    @ApiOperation(value = "Retonar uma mensagem estática de Home Page",
+            notes = "Este endpoint retorna uma mensagem estática de Home Page")
     public String getHomePage(){
         return "Bem vindo(a) a página inicial!!";
     }
