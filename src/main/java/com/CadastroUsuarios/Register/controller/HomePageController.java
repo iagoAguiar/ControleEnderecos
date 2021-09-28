03cc7796c3e7963f56e157a110269bf6ace4f7ae
iagoAguiar <iagoaguiar202@gmail.com>
2021-09-27 21:11:17 -0300

    Refatorada a Home Page
    A Home Page foi refatorada para redirecionar para o Swagger.

--- src/main/java/com/CadastroUsuarios/Register/controller/HomePageController.java
+++ src/main/java/com/CadastroUsuarios/Register/controller/HomePageController.java
@@ -5,15 +5,19 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequestMapping("/")
 @Api(value = "HomePage",  tags = { "HomePage" })
 public class HomePageController {
+
+
     @GetMapping
-    @ApiOperation(value = "Retonar uma mensagem estática de Home Page",
-            notes = "Este endpoint retorna uma mensagem estática de Home Page")
-    public String getHomePage(){
-        return "Bem vindo(a) a página inicial!!";
+    @ApiOperation(value = "Redireciona para o Swagger",
+            notes = "Este endpoint redireciona o get da página inicial para o Swaager")
+    public RedirectView goToSwagger() {
+            return new RedirectView("swagger-ui.html");
     }
 }
